9632e57dc5aadf8f9357b7b6d606b31283679f8f
chatea <cha122977@gmail.com>
2015-09-17 12:19:57 +0800

    Create skeleton of index.html

--- src/index.js
+++ src/index.js
@@ -1,4 +1,32 @@
 React.render(
-  <h1>The follower home page</h1>,
+  <header>
+  <h1>The follower home page</h1>
+  </header>,
   document.getElementById('title')
 );
+
+var Page = React.createClass({
+  render: function() {
+    return (
+      <div className="page">
+        {this.props.title}
+      </div>
+    );
+  }
+});
+
+var Pages = React.createClass({
+  render: function() {
+    var pages = [];
+    for (var value in this.props.titles) {
+      pages.push(<Page title={this.props.titles[value]} />);
+    }
+    return <div>{pages}</div>;
+  }
+})
+
+var titles=['xxx','yyy','zzz'];
+React.render(
+  <Pages titles={titles}/>,
+  document.getElementById('pages')
+)
