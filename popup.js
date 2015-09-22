3a5a83a2ab4ac5f5b6d06e3e32f43ff3cbb28ba9
chatea <cha122977@gmail.com>
2015-09-22 16:25:43 +0800

    Remove old popup.js

--- popup.js
+++
@@ -1,8 +0,0 @@
-function openFollowerPage() {
-    var myid = chrome.runtime.id;
-    chrome.tabs.create({url:"index.html"});
-}
-
-document.addEventListener('DOMContentLoaded', function() {
-  openFollowerPage();
-});
