7847d591fc346cface33f9433c0e62b6187a3e58
chatea <cha122977@gmail.com>
2015-09-22 11:56:52 +0800

    Add UUID function
    FIXME: cannot use UUID to save url

--- src/popup.js
+++ src/popup.js
@@ -6,10 +6,30 @@ function openFollowerPage() {
 
 function registerCurrentPage() {
   // TODO implement this
-  alert("Has not be implemented");
+  chrome.tabs.query({'active': true}, function(tabs) {
+    if (tabs.length == 0) {
+        message("There is no page to register");
+    }
+
+    var url = tabs[0].url;
+    var uuid = createUUID();
+
+    chrome.storage.sync.set({uuid: url}, function() {
+      chrome.storage.sync.get(uuid, function(urls) {
+        // FIXME the URL is undefined.
+        // Maybe we cannot use UUID to save url?
+        alert(urls[uuid]);
+      });
+    });
+  });
 }
 
-
+function createUUID() {
+  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+    var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
+    return v.toString(16);
+  });
+}
 // TODO unregisterCurrentPage() and its React class
 
 
